ab206fd8a02fabdaeceb34e6dc33c217115fe980
VittorioSchiavon <vittorio.schiavon@studenti.unipd.it>
2021-04-12 19:08:47 +0200

    added movement

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function move(){
-    document.getElementById("player").style.top=5+"vmin";
+var player = document.getElementById("player");
+var x = parseFloat(window.getComputedStyle(player).getPropertyValue("left"));
+var y = parseFloat(window.getComputedStyle(player).getPropertyValue("top"));
+var speed = 5;
+console.log(y);
+console.log(x);
+
+function moveLeft(moveSpeed){
+    console.log(x);
+    x+=moveSpeed;
+    player.style.left = x + "px";
+    
 }
+function moveTop(moveSpeed){
+    console.log(y);
+    y+=moveSpeed;
+    player.style.top = y +"px";
+}
+
+
+document.addEventListener("keydown", event=>{
+    if(event.key==="ArrowLeft"|| event.key==="a") moveLeft(-speed);
+    if(event.key==="ArrowRight"|| event.key==="d") moveLeft(+speed);
+    if(event.key==="ArrowUp"|| event.key==="w") moveTop(-speed);
+    if(event.key==="ArrowDown"|| event.key==="s") moveTop(+speed);
+})
