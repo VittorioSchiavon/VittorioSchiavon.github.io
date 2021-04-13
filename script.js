24e436673beb918bb667cc0ddb23dc714a414119
VittorioSchiavon <vittorio.schiavon@studenti.unipd.it>
2021-04-13 09:27:43 +0200

    background and animations

--- script.js
+++ script.js
@@ -1,26 +1,75 @@
+var background = document.getElementById("background");
 var player = document.getElementById("player");
-var x = parseFloat(window.getComputedStyle(player).getPropertyValue("left"));
-var y = parseFloat(window.getComputedStyle(player).getPropertyValue("top"));
-var speed = 5;
-console.log(y);
-console.log(x);
+var unit = parseFloat(window.getComputedStyle(player).getPropertyValue("width"));
+var centreX= unit*10;
+var centreY= unit*18;
+var x = parseFloat(window.getComputedStyle(background).getPropertyValue("left"));
+var y = parseFloat(window.getComputedStyle(background).getPropertyValue("top"));
+var backgroundWidth=parseFloat(window.getComputedStyle(background).getPropertyValue("width"));
+var backgroundHeight=parseFloat(window.getComputedStyle(background).getPropertyValue("height"));
+var speed = parseFloat(window.getComputedStyle(player).getPropertyValue("width"))/8;
+var imageX=0;
+var imageY=0;
+var changeImage=true;
+
+
+setInterval(function(){changeImage=true;},100);
+
+
+function shiftImage() {
+    if(changeImage){
+        player.style.backgroundImage=`url(images/Vittorio-0-${imageX}.png)`;
+        imageX++;
+        if(imageX==4){
+            imageX=0;
+        }
+        changeImage=false;
+    }
+}
+
+
+document.addEventListener("keyup", event=>{
+    imageX=0;
+    shiftImage();
+})
+
 
 function moveLeft(moveSpeed){
     console.log(x);
-    x+=moveSpeed;
-    player.style.left = x + "px";
-    
+    if(checkBorders(moveSpeed,0)){
+        x+=moveSpeed;
+        background.style.left = x + "px";
+        
+    }
+    shiftImage();
 }
 function moveTop(moveSpeed){
     console.log(y);
-    y+=moveSpeed;
-    player.style.top = y +"px";
+    if(checkBorders(0,moveSpeed)){
+        y+=moveSpeed;
+        background.style.top = y +"px";
+        
+    }
+    shiftImage();
 }
 
 
 document.addEventListener("keydown", event=>{
-    if(event.key==="ArrowLeft"|| event.key==="a") moveLeft(-speed);
-    if(event.key==="ArrowRight"|| event.key==="d") moveLeft(+speed);
-    if(event.key==="ArrowUp"|| event.key==="w") moveTop(-speed);
-    if(event.key==="ArrowDown"|| event.key==="s") moveTop(+speed);
-})
+    if(event.key==="ArrowLeft"|| event.key==="a") moveLeft(+speed);
+    if(event.key==="ArrowRight"|| event.key==="d") moveLeft(-speed);
+    if(event.key==="ArrowUp"|| event.key==="w") moveTop(+speed);
+    if(event.key==="ArrowDown"|| event.key==="s") moveTop(-speed);
+});
+
+
+function checkBorders(movementLeft, movementTop){
+    //return true;
+    console.log(backgroundHeight);
+    if(movementLeft){
+        if(x+movementLeft>unit*5-unit/2 || x+movementLeft<-backgroundWidth+unit*10-unit*5+unit/2) return false;
+    };
+    if(movementTop){
+        if(y+movementTop>unit*4 || y+movementTop<-backgroundHeight+unit*8-unit*4+unit/2) return false;
+    };
+    return true;
+}
